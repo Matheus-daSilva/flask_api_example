853287d52f8e36e86770989c2b8117baacf05ac4
Matheus da Silva <matheus2001silva@live.com>
2024-05-18 20:47:44 -0300

    feat: add get_item_in_store endpoint

--- app.py
+++ app.py
@@ -42,3 +42,9 @@ def get_store(name):
             return store
     return {"message": "Store no found"}, 404
 
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store no found"}, 404
