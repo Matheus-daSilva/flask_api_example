b97dc6b2b00a5d64745800530df1b1bfb39b20f9
Matheus da Silva <matheus2001silva@live.com>
2024-05-24 16:16:13 -0300

    fix: change stores iteration taking the stores values

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ def create_store():
             400,
             message="Bad request. Ensure 'name' is included in the JSON payload."
         )
-    for store in stores["name"] == store["name"]:
-        abort(400, message=f"Store already exists.")
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
 
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
