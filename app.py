b38110ecbc264bbd7c9ef2a164d4627d8d99abe5
Matheus da Silva <matheus2001silva@live.com>
2024-05-26 14:41:27 -0300

    feat: add a context to create database tables

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask
 from flask_smorest import Api
 
+from db import db
+import models
+
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
@@ -17,8 +20,13 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
     api = Api(app)
+
+    with app.app_context():
+        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
