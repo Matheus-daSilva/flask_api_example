c37ba6c02d7dc9dad014f5fb581418231daf7806
Matheus da Silva <matheus2001silva@live.com>
2024-05-26 16:10:10 -0300

    refactor: method get o Item class

--- resources/item.py
+++ resources/item.py
@@ -13,10 +13,8 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Store not found")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
         try:
