9c0c7a520d6fcdbc0b8e0f6bf77270cdf20d733a
Matheus da Silva <matheus2001silva@live.com>
2024-05-30 19:01:55 -0300

    fix: delete a store

--- resources/store.py
+++ resources/store.py
@@ -18,8 +18,10 @@ class Store(MethodView):
         return store
 
     def delete(self, store_id):
-        store = StoreModel.query.get_or_404(store_id) 
-        raise NotImplementedError("Deleting a store is not implemented.")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
 
 
 @blp.route("/store")
