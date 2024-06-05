81fff3fc2c504aa3f68ca053db0a2cbd25a54ac1
Matheus da Silva <matheus2001silva@live.com>
2024-06-05 15:57:04 -0300

    feat: add blocklist set

---
+++ blocklist.py
@@ -0,0 +1,9 @@
+"""
+blockist.py
+
+This file just contains the blocklist of the JW tokens. It will be imported by
+app and the logout resouce so that tokens can be added to the blocklist when the
+user logs out.
+"""
+
+BLOCKLIST = set()
